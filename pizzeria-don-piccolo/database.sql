64e9cfe2d74823f46804349040cda23acba76b45
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-12-01 08:58:25 -0500

    feat: :hammer: data files first part

--- pizzeria-don-piccolo/database.sql
+++ pizzeria-don-piccolo/database.sql
@@ -1,5 +1,15 @@
 create database pizzeria_don_piccolo;
 Use pizzeria_don_piccolo;
 
+create table Gesion_clientes;
+
+create table Gestion_pizza;
+
+create table Gestion_pedido;
+
+create table Gestion_repartidores;
+
+create table Gestion_domicilios;
+
 -- Y no se que mas XD
 -- Por fin Libre Albedrio en los proyectos.
